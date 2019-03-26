bf5149ef41a163ed3d77cd5d4dd011871c88f02e
Peter Hinge <contactpeterhinge@gmail.com>
2019-03-26 23:01:33 +0800

    added rulebased computer

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,18 +1,20 @@
 import random
+import time
 
 import pygame
+
 
 pygame.init()
 
 
-def setup(n, mines):
+def setup(n, mines):  # Sets up underlying game table
     table = generate_table(n)
     table = add_mines(table, mines)
     table = adjust_table(table)
     return table
 
 
-def generate_table(n):
+def generate_table(n):  # Creates a table with the specified parameters
     return [[0] * n for i in range(n)]
 
 
@@ -32,10 +34,9 @@ def adjust_table(table):  # Checks the surrounding cells clockwise and adjusts t
     for x in range(len(table)):
         for y in range(len(table[x])):
             if table[x][y] == 9:
-
                 adjacent_tiles = get_adjacent_tiles(x, y)
                 for tile in adjacent_tiles:
-                    if is_inside_table(table, tile[0], tile[1]):
+                    if is_inside_table(len(table), tile[0], tile[1]):
                         if not is_bomb(table, tile[0], tile[1]):
                             table[tile[0]][tile[1]] += 1
 
@@ -43,16 +44,16 @@ def adjust_table(table):  # Checks the surrounding cells clockwise and adjusts t
 
 
 def get_adjacent_tiles(x, y):
-    neighbour = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]]
+    neighbours = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]]
     adjacent_tiles = []
-    for coordinate in neighbour:
+    for coordinate in neighbours:
         adjacent_tiles.append([x + coordinate[0], y + coordinate[1]])
 
     return adjacent_tiles
 
 
-def is_inside_table(table, x, y):
-    return 0 <= x <= len(table) - 1 and 0 <= y <= len(table) - 1
+def is_inside_table(table_length, x, y):
+    return 0 <= x <= table_length - 1 and 0 <= y <= table_length - 1
 
 
 def is_bomb(table, x, y):
@@ -88,11 +89,42 @@ def open_game(lst, square):
     x, y = square.x // 20, square.y // 20
     adjacent_tiles = get_adjacent_tiles(x, y)
     for tile in adjacent_tiles:
-        if 0 <= tile[0] <= size_of_board - 1 and 0 <= tile[1] <= size_of_board - 1:
+        if is_inside_table(size_of_board, tile[0], tile[1]):
             if not lst[tile[0]][tile[1]].visible and not lst[tile[0]][tile[1]].flag:
                 lst[tile[0]][tile[1]].visible = True
-                if lst[tile[0]][tile[1]].val == 0:
+                if lst[tile[0]][tile[1]].val == 0:  # Recursive step that ensures all whites spaces are opened
                     open_game(lst, lst[tile[0]][tile[1]])
+    return lst
+
+
+def ai_player(lst, square):
+    x, y = square.x // 20, square.y // 20
+    adjacent_tiles = get_adjacent_tiles(x, y)
+    adjacent_mines = square.val
+    adjacent_unopened_squares = 0
+    for tile in adjacent_tiles:
+        if is_inside_table(size_of_board, tile[0], tile[1]):
+            if not lst[tile[0]][tile[1]].visible:
+                adjacent_unopened_squares += 1
+    if adjacent_mines == adjacent_unopened_squares:  # First Rule: Flag if unopened squares and adjacent mines are same
+        for tile in adjacent_tiles:
+            if is_inside_table(size_of_board, tile[0], tile[1]):
+                if not lst[tile[0]][tile[1]].visible:
+                    pygame.mouse.set_pos([tile[0] * 20 + 10, tile[1] * 20 + 10])
+                    time.sleep(3)
+                    lst[tile[0]][tile[1]].flag = True
+    adjacent_flags = 0
+    for tile in adjacent_tiles:
+        if is_inside_table(size_of_board, tile[0], tile[1]):
+            if lst[tile[0]][tile[1]].flag:
+                adjacent_flags += 1
+    if adjacent_mines == adjacent_flags:  # Second Rule: Opens squares if adjacent flags and adjacent mines are same
+        for tile in adjacent_tiles:
+            if is_inside_table(size_of_board, tile[0], tile[1]):
+                if not lst[tile[0]][tile[1]].visible and not lst[tile[0]][tile[1]].flag:
+                    pygame.mouse.set_pos([tile[0] * 20 + 10, tile[1] * 20 + 10])
+                    time.sleep(3)
+                    lst[tile[0]][tile[1]].visible = True
     return lst
 
 
@@ -127,19 +159,42 @@ def game(size, mines):
             lst[i // 20] += [Square(i, j, 20, 20, spil.board, (i // 20, j // 20))]
             screen.blit(grey, (i, j))
 
+    ai_on = False
+
     exit_game = False
     while not exit_game:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+
+            if event.type == pygame.QUIT:  # Quit game event
                 exit_game = True
                 pygame.quit()
 
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
+            elif event.type == pygame.KEYDOWN:  # Keyboard pressed event
+
+                if event.key == pygame.K_r:  # Restart game event
                     exit_game = True
                     restart(size, mines)
 
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                elif event.key == pygame.K_c:  # Turn AI on/off event
+                    if not ai_on:
+                        ai_on = True
+                    elif ai_on:
+                        ai_on = False
+
+            elif ai_on:
+                for i in lst:
+                    for j in i:
+                        if j.visible:
+                            ai_player(lst, j)
+                            time.sleep(3)
+                            print("3")
+
+
+
+
+
+
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left mouse-click event
                 for i in lst:
                     for j in i:
                         r = pygame.rect.Rect(pygame.mouse.get_pos(), (1, 1))
@@ -154,7 +209,7 @@ def game(size, mines):
                                     j.visible = open_game(lst, j)
                                     j.visible = True
 
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:  # Right mouse-click event
                 for i in lst:
                     for j in i:
                         r = pygame.rect.Rect(pygame.mouse.get_pos(), (1, 1))
